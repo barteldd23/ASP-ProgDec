532ebf4ca054694ad40e237547de46e1cb8ae254
Dean Bartel <300089145@fvtc.edu>
2023-09-12 14:16:23 -0500

    added utProgram loadtest and insertTest

--- DDB.ProgDec.PL.Test/UnitTest1.cs
+++ DDB.ProgDec.PL.Test/UnitTest1.cs
@@ -9,5 +9,22 @@ namespace DDB.ProgDec.PL.Test
             ProgDecEntities dc = new ProgDecEntities();
             Assert.AreEqual(16, dc.tblPrograms.Count());
         }
+
+        [TestMethod]
+        public void InsertTest() 
+        {
+            var dc = new ProgDecEntities();
+
+            // Make an Entity
+            tblProgram entity = new tblProgram();
+            entity.DegreeTypeId = 2;
+            entity.Description = "Basket Weaving";
+            entity.Id = -99;
+
+            //Add entity to database
+            dc.tblPrograms.Add(entity);
+            // commit the changes
+            int result = dc.SaveChanges();
+        }
     }
 }
