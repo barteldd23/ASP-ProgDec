132f7089b957db78313d84471a14fab84ba1c866
Dean Bartel <300089145@fvtc.edu>
2023-09-14 13:56:02 -0500

    Added Code for utDegreeType and ut Declaration

---
+++ DDB.ProgDec.PL.Test/utDeclaration.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB.ProgDec.PL.Test
+{
+    [TestClass]
+    public class utDeclaration
+    {
+        protected ProgDecEntities dc;
+        protected IDbContextTransaction transaction;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dc = new ProgDecEntities();
+            transaction = dc.Database.BeginTransaction();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Rollback();
+            transaction.Dispose();
+            dc = null;
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            Assert.AreEqual(4, dc.tblDeclarations.Count());
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+
+            // Make an Entity
+            tblDeclaration entity = new tblDeclaration();
+            entity.StudentId = 1655466;
+            entity.ProgramId = 4;
+            entity.ChangeDate = DateTime.Now;
+            entity.Id = -99;
+
+            //Add entity to database
+            dc.tblDeclarations.Add(entity);
+            // commit the changes
+            int result = dc.SaveChanges();
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            // Select * from tblDeclaration - Use the first one
+            tblDeclaration entity = dc.tblDeclarations.FirstOrDefault();
+
+            // Change property values
+            entity.StudentId = 5;
+            entity.ProgramId = 1;
+
+            int result = dc.SaveChanges();
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            // select * from tblDeclaration where id = 4;
+            tblDeclaration entity = dc.tblDeclarations.Where(e => e.Id == 4).FirstOrDefault();
+
+            dc.tblDeclarations.Remove(entity);
+            int result = dc.SaveChanges();
+            Assert.AreNotEqual(result, 0);
+        }
+    }
+}
---
+++ DDB.ProgDec.PL.Test/utDegreeType.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB.ProgDec.PL.Test
+{
+    [TestClass]
+    public class utDegreeType
+    {
+        protected ProgDecEntities dc;
+        protected IDbContextTransaction transaction;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dc = new ProgDecEntities();
+            transaction = dc.Database.BeginTransaction();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            transaction.Rollback();
+            transaction.Dispose();
+            dc = null;
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            Assert.AreEqual(3, dc.tblDegreeTypes.Count());
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+
+            // Make an Entity
+            tblDegreeType entity = new tblDegreeType();
+            entity.Description = "Masters";
+            entity.Id = -99;
+
+            //Add entity to database
+            dc.tblDegreeTypes.Add(entity);
+            // commit the changes
+            int result = dc.SaveChanges();
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            // Select * from tblDegreeType - Use the first one
+            tblDegreeType entity = dc.tblDegreeTypes.FirstOrDefault();
+
+            // Change property values
+            entity.Description = "Masters";
+
+
+            int result = dc.SaveChanges();
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void DeleteTest()
+        {
+            // select * from tblDegreeType where id = 4;
+            tblDegreeType entity = dc.tblDegreeTypes.Where(e => e.Id == 2).FirstOrDefault();
+
+            dc.tblDegreeTypes.Remove(entity);
+            int result = dc.SaveChanges();
+            Assert.AreNotEqual(result, 0);
+        }
+    }
+}
