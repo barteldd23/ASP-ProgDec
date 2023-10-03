295fe8584e246157a241ddf41e6b7e94914a2a63
Dean Bartel <300089145@fvtc.edu>
2023-10-03 14:14:22 -0500

    Added Band Controller, and Indext and details views

---
+++ DDB.Bands.UI/Controllers/BandController.cs
@@ -0,0 +1,100 @@
+﻿using DDB.Bands.UI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDB.Bands.UI.Controllers
+{
+    public class BandController : Controller
+    {
+
+        Band[] bands;
+
+        public void LoadBands()
+        {
+            bands = new Band[]
+            {
+                new Band{Id = 1, Name="Eric Clapton", Genre="Blues", DateFounded = new DateTime(1978,4,3)},
+                new Band{Id = 2, Name="Linkin Park", Genre="Alternative Rock", DateFounded = new DateTime(1996,2,15)},
+                new Band{Id = 3, Name="Disturbed", Genre="Rock", DateFounded = new DateTime(1994,10,13)}
+            };
+        }
+
+        // GET: BandController
+        public ActionResult Index()
+        {
+            LoadBands();
+            return View(bands);
+        }
+
+        // GET: BandController/Details/5
+        public ActionResult Details(int id)
+        {
+            LoadBands();
+            Band band = bands.FirstOrDefault(b => b.Id == id);
+            return View(band);
+        }
+
+        // GET: BandController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BandController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BandController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BandController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BandController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BandController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DDB.Bands.UI/Models/Band.cs
@@ -0,0 +1,11 @@
+﻿namespace DDB.Bands.UI.Models
+{
+    public class Band
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime DateFounded { get; set; }
+        public string Image { get; set; }
+    }
+}
