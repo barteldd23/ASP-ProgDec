2a3871fb7d0c1ee22275f7e3f90ba46a87fc91ee
Dean Bartel <300089145@fvtc.edu>
2023-11-28 14:19:42 -0600

    added shopping cart code

---
+++ DDB.ProgDec.BL.Models/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB.ProgDec.BL.Models
+{
+    public class ShoppingCart
+    {
+        // Declaration application specific - Declaration Cost
+        const double ITEM_COST = 120.03;
+
+        public List<Declaration> Items {  get; set; } = new List<Declaration>();
+        public int NumberOfItems { get {  return Items.Count; } }
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double SubTotal { get { return Items.Count *  ITEM_COST; } }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double Tax { get { return SubTotal * .055; } }
+
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double Total { get { return SubTotal + Tax; } }
+    }
+}
---
+++ DDB.ProgDec.BL/ShoppingCartManager.cs
@@ -0,0 +1,40 @@
+﻿using DDB.ProgDec.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDB.ProgDec.BL
+{
+    public static class ShoppingCartManager
+    {
+        public static void Add(ShoppingCart cart, Declaration declaration)
+        {
+            if(cart != null) { cart.Items.Add(declaration); }
+        }
+
+        public static void Remove(ShoppingCart cart, Declaration declaration)
+        {
+            if (cart != null) { cart.Items.Remove(declaration); }
+        }
+
+        public static void Checkout (ShoppingCart cart)
+        {
+            // Make a new order
+            // Set the Order fields as needed.
+            
+            // foreach(Movie item in cart.Items)
+            //
+            // Make a new orderItem
+            // Set the OrderItem fields from the item
+            // order.OrderItems.Add(orderItem)
+            
+            // OrderManager.Insert(order)
+
+            // Decrement the tblMovie.InStkQty appropriately.
+
+            cart = new ShoppingCart();
+        }
+    }
+}
