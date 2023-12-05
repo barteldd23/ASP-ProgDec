147d1f05319d6bf0672476aab46f4863956b0220
Dean Bartel <300089145@fvtc.edu>
2023-12-05 14:22:32 -0600

    Made API Project and controllers

---
+++ DDB.ProgDec.API/Controllers/DeclarationController.cs
@@ -0,0 +1,72 @@
+﻿using DDB.ProgDec.BL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DDB.ProgDec.BL.Models;
+using Microsoft.VisualStudio.Web.CodeGeneration.Design;
+
+namespace DDB.ProgDec.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeclarationController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<BL.Models.Declaration> Get()
+        {
+            return DeclarationManager.Load();
+        }
+
+        // tell it here in the httpget that there is a parameter
+        [HttpGet("{id}")]
+        public BL.Models.Declaration Get(int id)
+        {
+            return DeclarationManager.LoadByID(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] BL.Models.Declaration declaration)
+        {
+            try
+            {
+                int results = DeclarationManager.Insert(declaration);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put( int id, [FromBody] BL.Models.Declaration declaration)
+        {
+            try
+            {
+                int results = DeclarationManager.Update(declaration);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                int results = DeclarationManager.Delete(id);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+
+
+        
+    
+}
---
+++ DDB.ProgDec.API/Controllers/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using DDB.ProgDec.BL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DDB.ProgDec.BL.Models;
+using Microsoft.VisualStudio.Web.CodeGeneration.Design;
+
+namespace DDB.ProgDec.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<BL.Models.Student> Get()
+        {
+            return StudentManager.Load();
+        }
+
+        // tell it here in the httpget that there is a parameter
+        [HttpGet("{id}")]
+        public BL.Models.Student Get(int id)
+        {
+            return StudentManager.LoadByID(id);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] BL.Models.Student dstudent)
+        {
+            try
+            {
+                int results = StudentManager.Insert(dstudent);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put( int id, [FromBody] BL.Models.Student dstudent)
+        {
+            try
+            {
+                int results = StudentManager.Update(dstudent);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                int results = StudentManager.Delete(id);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+
+
+        
+    
+}
